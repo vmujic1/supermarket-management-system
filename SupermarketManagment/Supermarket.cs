68c15c836de1e385c8050ce467d6713f3f4e5cf0
vmujic1 <vmujic1@etf.unsa.ba>
2024-01-30 18:03:46 +0100

    Added method to add new employees in suprermarket

--- SupermarketManagment/Supermarket.cs
+++ SupermarketManagment/Supermarket.cs
@@ -11,14 +11,16 @@ namespace SupermarketManagment
         private string ime;
         private string lokacija;
         private Asortiman asortiman;
+        private List<Uposlenik> uposlenici;
 
         public string Ime { get =>  ime; set => ime = value; }
         public string Lokacija { get => lokacija; set => lokacija = value; }
         public Asortiman Asortiman { get => asortiman; set => asortiman = value; }
-
+        public List<Uposlenik> Uposlenici { get => uposlenici; set => uposlenici = value; }
 
         public Supermarket()
         {
+            uposlenici = new List<Uposlenik>();
             
         }
         public Supermarket(string ime, string lokacija)
@@ -26,6 +28,11 @@ namespace SupermarketManagment
             this.Ime = ime;
             this.Lokacija = lokacija;
             
+        }
+
+        public void dodajUposlenika(Uposlenik uposlenik)
+        {
+            Uposlenici.Add(uposlenik);
         }
 
     }
