739fe1494d377475ea729f3af5e51f3da58e8f45
vmujic1 <vmujic1@etf.unsa.ba>
2024-01-31 13:42:26 +0100

    Added login fuctionality

--- SupermarketManagment/Program.cs
+++ SupermarketManagment/Program.cs
@@ -1,6 +1,8 @@
 ﻿
 
 using SupermarketManagment;
+using System.Xml.Linq;
+
 class Program
 {
     static void Main(string[] args)
@@ -14,7 +16,52 @@ class Program
 
         uposlenik.talk();
 
+
         Console.WriteLine(supermarket.Ime + ' '+ supermarket.Lokacija);
+
+
+
+        string direktoriji = Directory.GetCurrentDirectory();
+        string direktorijiBezDodatnihFoldera = Path.Combine(direktoriji, "..", "..", "..");
+
+        string relativnaPutanja = Path.Combine(direktorijiBezDodatnihFoldera, "UposleniciData.xml");
+        string apsolutnaPutanja = Path.GetFullPath(relativnaPutanja);
+
+
+        Console.Write("Unesite korisničko ime: ");
+        string username = Console.ReadLine();
+
+        Console.Write("Unesite lozinku: ");
+        string password = Console.ReadLine();
+
+
+
+
+        bool uspjesnaPrijava = ProvjeraPrijave(apsolutnaPutanja, username, password);
+
+        if (uspjesnaPrijava)
+        {
+            Console.WriteLine("Uspešno ste se prijavili!");
+        }
+        else
+        {
+            Console.WriteLine("Prijavljivanje nije uspelo. Proverite korisničko ime i lozinku.");
+        }
+
+
+        static bool ProvjeraPrijave(string xmlFilePath, string username, string password)
+        {
+            XDocument xdoc = XDocument.Load(xmlFilePath);
+
+            string element = "user";
+
+            
+            var user = xdoc.Descendants(element)
+                           .FirstOrDefault(u => (string)u.Element("username") == username && (string)u.Element("password") == password);
+
+            return user != null;
+        }
+
     }
 }
 
--- SupermarketManagment/obj/Debug/net6.0/SupermarketManagment.AssemblyInfo.cs
+++ SupermarketManagment/obj/Debug/net6.0/SupermarketManagment.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SupermarketManagment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f629875927d8c19988f3e149ac6a4b9bebdf105")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68c15c836de1e385c8050ce467d6713f3f4e5cf0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SupermarketManagment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SupermarketManagment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
