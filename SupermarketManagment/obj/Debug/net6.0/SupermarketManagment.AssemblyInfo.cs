966f425ce451f527fd7e8cc69866f4885bcab919
vmujic1 <vmujic1@etf.unsa.ba>
2024-02-04 19:53:43 +0100

    Added gitignore

--- SupermarketManagment/obj/Debug/net6.0/SupermarketManagment.AssemblyInfo.cs
+++ SupermarketManagment/obj/Debug/net6.0/SupermarketManagment.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SupermarketManagment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9f16a6b516fa4cc91440442a9ab90b96d055718")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SupermarketManagment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SupermarketManagment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
